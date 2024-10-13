d7747214b992247f326e938fc6952f9a125497ad
Harsamerveer Singh <harsamerveersingh@gmail.com>
2024-10-13 13:01:02 -0700

    Another minor change in App.js; removed extra code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import ProfileScanner from './ProfileScanner';
-import serverforprofilescanner from '../functions/server';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <ProfileScanner />
-                <serverforprofilescanner />
             </header>
         </div>
     );
